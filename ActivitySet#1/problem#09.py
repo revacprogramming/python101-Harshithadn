1d255f3d41a5596d479607de4ac582009e71aa65
Harshitha DN <97090084+Harshithadn@users.noreply.github.com>
2022-06-03 03:20:48 +0000

    9

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,13 @@
-# Lists
+file_name = input("Enter file name: ")
+file_handling = open(file_name)
+empty_list = list()
+filled_list = list()
 
-file_name= input("Enter file name ")
+for line in file_handling:
+    line = line.strip().split()
+    for word in line:
+        if word not in empty_list:
+            empty_list.append(word)
+            filled_list = sorted(empty_list)
+
+print(filled_list)
