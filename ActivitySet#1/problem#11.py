2eb33e370c6621a2c6271bd895c26ac26517379d
Harshitha DN <97090084+Harshithadn@users.noreply.github.com>
2022-07-01 13:14:08 +0000

    11

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,16 @@
 # Tuples
 
-filename = "dataset/mbox-short.txt"
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+file1 = open(name)
+dct = dict()
+for line in file1:
+    if line.startswith("From "): 
+        lst = line.split()
+        num = lst[5]
+        time = num.split(":")
+        dct[time[0]] = dct.get(time[0],0) + 1
+ftime = sorted(dct.items())
+for (k,v) in ftime:
+    print(k,v)
